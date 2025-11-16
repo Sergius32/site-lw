9ac419e35859afd8f824a44a2ca5c13e3ee22745
Sergius32 <Xxfosters@gmail.com>
2025-11-16 23:09:40 +0300

    новые шрифты, бэкапы

---
+++ backup_sphere/db.php
@@ -0,0 +1,7 @@
+<?php
+const DB_HOST = '77.222.61.179';
+const DB_USER = 'xxfostersg_spher';
+const DB_PASSWORD = 'R8BLPtVA6WQB2AZ^';
+const DB_NAME = 'xxfostersg_spher';
+const DB_PORT = '3306';
+const CHARSET = 'utf8';
